e8b430c77adbd80673f1986934431a95da3e8247
sannydo <sanny.do94@gmail.com>
2020-12-14 15:51:23 -0600

    commit tree aec4dae186f1bda3522d34855201fda10e4e340d

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards ( namesArray, event) {
+    let thankYouCards = []
+    for ( let i = 0; i< namesArray.length; i++){
+        thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouCards
+}
+
+function countDown(countDown){
+    while (countDown > 0) {
+        console.log(countDown);
+        countDown -= 1;
+    }
+    console.log(countDown);
+    }
+    console.log(writeCards(["jessy", "james","and meow too"],"poke"))
+
+
